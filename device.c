39d23a002ea0521f0fa5ff3484ee84dab859f423
Sakthivel Velumani <sakthi92@gmail.com>
2024-10-16 02:07:06 +0000

    mqtt works

--- device.c
+++ device.c
@@ -13,18 +13,20 @@
 #include <time.h>
 #include <unistd.h>
 
+#include "MQTTClient.h"
 #include "aes/aes.h"
 #include "common.h"
 #include "server.h"
 #include "spi_device/spi.h"
 #include "timer.h"
+#include <json-c/json.h>
 
 #define CONSUMER "Motor controller"
 #define SERVER_IP "192.168.193.106"
 #define DEVICE_ID 1
 
 #define STARTER_BUTTON_TIMER 200
-#define MSG_A_PERIOD_S 10
+#define MSG_A_PERIOD_S 30
 #define USB_POWER_RESET_TIME 5
 #define GPIO_CHIP_0 "gpiochip0"
 #define GPIO_CHIP_1 "gpiochip1"
@@ -37,10 +39,18 @@
 #define MAX_CONN_ERR 3
 #define USB_POWER_PIN 26
 
+#define ADDRESS "tcp://192.168.193.106:1883"
+#define CLIENTID "Kelakadu"
+#define TOPIC "voltage/"
+#define SUBS_TOPIC "command/"
+#define QOS 1
+#define TIMEOUT 10000L
+
 int client_socket = -1;
 timer_w_t motor_cutoff_timer;
 timer_w_t msg_A_timer;
 int conn_err_cnt = MAX_CONN_ERR;
+MQTTClient client;
 
 struct gpiod_chip *chip2;
 struct gpiod_line *valve0;
@@ -55,7 +65,7 @@ enum pin_dir { INPUT, OUTPUT };
 
 int get_rssi(void) { return -33; }
 
-int make_connection();
+MQTTClient *make_connection();
 
 int assign_pin(struct gpiod_chip *chip, struct gpiod_line **line, int pin,
                enum pin_dir dir) {
@@ -173,13 +183,120 @@ void send_msg_A(union sigval sv) {
       // try reconnecting
       close(client_socket);
       stop_timer(&msg_A_timer);
-      client_socket = make_connection();
+      // client_socket = make_connection();
       start_timer(MSG_A_PERIOD_S, MSG_A_PERIOD_S, send_msg_A, &msg_A_timer, -1);
       conn_err_cnt = 0;
     }
   } else {
     conn_err_cnt = 0;
   }
+}
+
+static float adc_to_coil_curr_conv(const uint16_t v) {
+  const float resolution = 0.05;
+  const float zeroCurVal = 1.65;
+  const float curVal = (v - zeroCurVal) / resolution;
+  return curVal;
+}
+
+static float adc_to_phase_volt_conv(const uint16_t v) {
+  const int r1 = 2e6;
+  const int r2 = 20e3;
+  const float pv = v * (r1 + r2) / r2;
+  return pv;
+}
+
+static void float_to_str(const float val, char *s, const int len) {
+  memset(s, 0, len);
+  snprintf(s, len, "%.2f", val);
+}
+
+static void send_phase_voltages(void) {
+  const float adc0 = adc_to_coil_curr_conv(get_avg_voltage(0, 5));
+  const float adc1 = adc_to_phase_volt_conv(get_avg_voltage(1, 5));
+  const float adc2 = adc_to_phase_volt_conv(get_avg_voltage(2, 5));
+  const float adc3 = adc_to_phase_volt_conv(get_avg_voltage(3, 5));
+
+  const int val0_state = gpiod_line_get_value(valve0);
+  const int val1_state = gpiod_line_get_value(valve1);
+  const int nc_state = gpiod_line_get_value(nc);
+  const int no_state = gpiod_line_get_value(no);
+  const int motor_state_val = gpiod_line_get_value(motor_state);
+  const int remTime = get_timer_state(&motor_cutoff_timer);
+
+  char str[10];
+  const int str_len = sizeof(str);
+
+  // Create a JSON object
+  struct json_object *jsonObj = json_object_new_object();
+
+  // Add data to the JSON object
+  snprintf(str, str_len, "%s", val0_state ? "close" : "open");
+  json_object_object_add(jsonObj, "valve0_state", json_object_new_string(str));
+
+  snprintf(str, str_len, "%s", val1_state ? "close" : "open");
+  json_object_object_add(jsonObj, "valve1_state", json_object_new_string(str));
+
+  snprintf(str, str_len, "%s", nc_state ? "close" : "open");
+  json_object_object_add(jsonObj, "nc_state", json_object_new_string(str));
+
+  snprintf(str, str_len, "%s", no_state ? "close" : "open");
+  json_object_object_add(jsonObj, "no_state", json_object_new_string(str));
+
+  snprintf(str, str_len, "%s", motor_state_val ? "off" : "on");
+  json_object_object_add(jsonObj, "motor_state", json_object_new_string(str));
+
+  snprintf(str, str_len, "%d", remTime);
+  json_object_object_add(jsonObj, "cutoff_time", json_object_new_string(str));
+
+  float_to_str(adc0, str, str_len);
+  json_object_object_add(jsonObj, "coil_current", json_object_new_string(str));
+
+  float_to_str(adc1, str, str_len);
+  json_object_object_add(jsonObj, "phase1", json_object_new_string(str));
+
+  float_to_str(adc2, str, str_len);
+  json_object_object_add(jsonObj, "phase2", json_object_new_string(str));
+
+  float_to_str(adc3, str, str_len);
+  json_object_object_add(jsonObj, "phase3", json_object_new_string(str));
+
+  // Convert the JSON object to a string
+  const char *jsonString = json_object_to_json_string(jsonObj);
+  printf("JSON Message: %s\n", jsonString);
+
+  // Prepare the MQTT message
+  // Publish a message
+  MQTTClient_message pubmsg = MQTTClient_message_initializer;
+  MQTTClient_deliveryToken token;
+  pubmsg.payload = (char *)jsonString;
+  pubmsg.payloadlen = (int)strlen(jsonString);
+  pubmsg.qos = QOS;
+  pubmsg.retained = 0;
+
+  // Publish the message to the ThingsBoard telemetry topic
+  MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+  printf("Publishing data: %s on topic %s\n", jsonString, TOPIC);
+
+  // Wait for the message to be delivered
+  MQTTClient_waitForCompletion(client, token, TIMEOUT);
+  printf("Message with delivery token %d delivered\n", token);
+
+  // Cleanup
+  json_object_put(jsonObj); // Decrement reference count and free if necessary
+}
+
+static void close_mqtt_conn(MQTTClient *c) {
+  // Disconnect from the broker
+  MQTTClient_disconnect(*c, 10000);
+  MQTTClient_destroy(c);
+}
+
+static void send_message(void) {
+  if (!MQTTClient_isConnected(client)) {
+    make_connection();
+  }
+  send_phase_voltages();
 }
 
 void start_motor() {
@@ -266,38 +383,114 @@ void receive_data(void) {
   }
 }
 
-int make_connection() {
-  int soc;
-  struct sockaddr_in server_address;
+// Function to control the motor
+void setMotorState(const char *state, int timeout) {
+  uint8_t curMotorState = gpiod_line_get_value(motor_state);
+  if (strcmp(state, "on") == 0) {
+    printf("Turning on the motor for %d minutes.\n", timeout);
+    // motor_state HI=OFF; LOW=ON
+    int remTimeSec = timeout * 60;
+    if ((timeout > 0) && (curMotorState)) {
+      start_motor();
+      start_timer(remTimeSec, 0, stop_motor_t, &motor_cutoff_timer, -1);
+    } else if ((timeout > 0) && (!curMotorState)) { // adjust timer
+      adjust_timer(remTimeSec, 0, stop_motor_t, &motor_cutoff_timer, -1);
+    }
+  } else if (strcmp(state, "off") == 0) {
+    if (!curMotorState) {
+      printf("Turning off the motor.\n");
+      stop_motor();
+      stop_timer(&motor_cutoff_timer);
+    }
+  }
+}
 
-  // Create a socket
-  soc = socket(AF_INET, SOCK_STREAM, 0);
-  if (soc == -1) {
-    perror("Error creating socket");
+// Function to control valve states
+// both valves cannot be closed at the same time
+void setValveState(const char *v0State) {
+  printf("Setting valve0 state %s\n", v0State);
+  if (strcmp(v0State, "close") == 0) {
+    gpiod_line_set_value(valve0, 0);
+    gpiod_line_set_value(valve1, 1);
+  } else if (strcmp(v0State, "open") == 0) {
+    gpiod_line_set_value(valve0, 1);
+    gpiod_line_set_value(valve1, 0);
+  }
+}
+
+// Message arrival callback function
+int messageArrived(void *context, char *topicName, int topicLen,
+                   MQTTClient_message *message) {
+  char *payload = message->payload;
+
+  // Parse the JSON message
+  struct json_object *parsed_json;
+  struct json_object *command, *state, *timeout;
+
+  parsed_json = json_tokener_parse(payload);
+  if (!json_object_object_get_ex(parsed_json, "command", &command)) {
+    printf("Error parsing command\n");
+    return 0;
+  }
+  if (!json_object_object_get_ex(parsed_json, "state", &state)) {
+    printf("Error parsing state\n");
+    return 0;
+  }
+  if (!json_object_object_get_ex(parsed_json, "timeout", &timeout)) {
+    printf("Error parsing timeout\n");
+    return 0;
   }
 
-  // Set up the server address structure
-  server_address.sin_family = AF_INET;
-  server_address.sin_port =
-      htons(SERVER_PORT); // Change this port number as needed
-  server_address.sin_addr.s_addr =
-      inet_addr(SERVER_IP); // Replace with the server's IP address or domain
+  // Interpret and execute the command
+  if (strcmp(json_object_get_string(command), "set_motor_state") == 0) {
+    setMotorState(json_object_get_string(state), json_object_get_int(timeout));
+  } else if (strcmp(json_object_get_string(command), "set_valve0_state") == 0) {
+    setValveState(json_object_get_string(state));
+  } else {
+    printf("Unknown command received.\n");
+  }
 
-  // Connect to the server
+  // Clean up
+  MQTTClient_freeMessage(&message);
+  MQTTClient_free(topicName);
+
+  return 1;
+}
+
+MQTTClient *make_connection() {
+  MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+  int rc;
+
+  // Create MQTT client
+  MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE,
+                    NULL);
+  MQTTClient_setCallbacks(client, NULL, NULL, messageArrived, NULL);
+  MQTTClient_setTraceLevel(MQTTCLIENT_TRACE_MAXIMUM);
+
+  // Set connection options
+  conn_opts.keepAliveInterval = 20;
+  conn_opts.cleansession = 1;
+  conn_opts.username = "kelakadu";
+  conn_opts.password = "Q3;'bw4RGK_#!Zb";
+
+  // Connect to the broker (only once)
   while (1) {
-    if (connect(soc, (struct sockaddr *)&server_address,
-                sizeof(server_address)) == -1) {
-      perror("Error connecting to server");
-      // maybe problem with modem. reset it
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
+      printf("Failed to connect, return code %d\n", rc);
       hard_reset_modem();
-      // give 30s to connect to network
-      usleep(30 * 1000 * 1000);
+      usleep(30 * 1000 * 1000); // wait for 30 sec
+      exit(EXIT_FAILURE);
     } else {
-      printf("connection with server etablished on soc %d\n", soc);
+      printf("MQTT connection established\n");
       break;
     }
   }
-  return soc;
+
+  // Subscribe to the topic
+  MQTTClient_subscribe(client, SUBS_TOPIC, QOS);
+
+  printf("Listening for messages on topic: %s\n", SUBS_TOPIC);
+  return &client;
 }
 
 int main() {
@@ -315,13 +508,13 @@ int main() {
     return -1;
   }
 
+  // set default valve states on restart
+  setValveState("open");
   // Send MSG A periodically
-  start_timer(MSG_A_PERIOD_S, MSG_A_PERIOD_S, send_msg_A, &msg_A_timer, -1);
-
-  receive_data();
-
-  // Close the socket
-  close(client_socket);
+  while (1) {
+    send_message();
+    usleep(MSG_A_PERIOD_S * 1000 * 1000);
+  }
 
   return 0;
 }
